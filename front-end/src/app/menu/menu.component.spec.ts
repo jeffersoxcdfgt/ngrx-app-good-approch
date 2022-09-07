00a8d8734afc7393956df1c26b0eed4ea3dfa1fe
Jefferson <jefferson.medina@dhigroupinc.com>
2022-09-07 09:59:43 -0500

    Update karma test unit casese

--- front-end/src/app/menu/menu.component.spec.ts
+++ front-end/src/app/menu/menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
+import { NullObjectToConvertPipe } from '../shared/pipes/null-object-value.pipe';
 
 import { MenuComponent } from './menu.component';
 
@@ -12,6 +13,7 @@ describe('MenuComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ 
         MenuComponent,
+        NullObjectToConvertPipe
       ],
       imports:[
         StoreModule.forRoot({}),
