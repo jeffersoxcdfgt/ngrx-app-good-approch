4d1ea142f689c4f19a649a5d69ceaf2917ed0c59
Jefferson <jefferson.medina@dhigroupinc.com>
2022-09-28 14:58:00 -0500

    create search on input bar

--- front-end/src/app/menu/arenas/arenas.component.ts
+++ front-end/src/app/menu/arenas/arenas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { select, Store } from '@ngrx/store'
-import { filter, Observable} from 'rxjs';
+import { filter, map, Observable} from 'rxjs';
 import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';;
 import { State } from 'src/app/shared/routing/id-reducer.reducer';
 import { selectId } from 'src/app/shared/routing/id.selectors';
@@ -11,6 +11,12 @@ import { arenasGetAll } from './store/actions/arenas.action';
 import { selectAllArenas } from './store/reducers/arenas.reducer';
 
 export const CLEANARRAY =  filter((val:Arena[]) => val.length >0 )
+
+export const FILTER_ARENA = (search:string) =>  map((arena:Arena[]) => {
+  return arena.filter((are:Arena)=> are.arenaTitle.toLocaleLowerCase().includes(search.toLowerCase()) || 
+                                    are.Capacity.toLocaleLowerCase().includes(search.toLowerCase()) ||     
+                                    are.About.toLocaleLowerCase().includes(search.toLowerCase()))
+})
 
 
 @Component({
@@ -22,11 +28,21 @@ export class ArenasComponent implements OnInit {
 
   selectedId$ = this.store.pipe(select(selectId));
   arenasList$ : Observable<Arena[]> = new Observable<Arena[]>();
+  searchTerm: string = '';
 
   constructor(private store :Store<State>,private dialog: MatDialog,) { }
 
   ngOnInit(): void { 
     this.store.dispatch(arenasGetAll());
+    this.arenasList$ = this.store.select(selectAllArenas).pipe(CLEANARRAY);
+  }
+
+  searchArena(data:string|any):void{    
+    this.arenasList$= this.store.select(selectAllArenas).pipe(CLEANARRAY,FILTER_ARENA(data.value));
+  }
+
+  resetSearch():void{
+    this.searchTerm = '';
     this.arenasList$ = this.store.select(selectAllArenas).pipe(CLEANARRAY);
   }
 
