61b765bf1305ef23699d817ed73cd728a575b70c
Jefferson <jefferson.medina@dhigroupinc.com>
2022-10-10 20:17:03 -0500

    Add new variable to environment

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   loginUrl:'https://jefferson-back.informati-k.com/api/login',
   arenaUrl:'http://localhost:3000',
-  teamUrl:'http://localhost:3000'
+  teamUrl:'http://localhost:3000',
+  playerUrl:'http://localhost:3000'
 };
