97244c7599efdb38555d0b706059c20088314e22
Jefferson <jefferson.medina@dhigroupinc.com>
2022-10-14 18:12:23 -0500

    map new list of items

--- front-end/src/app/menu/players/players-view/players-view.component.ts
+++ front-end/src/app/menu/players/players-view/players-view.component.ts
@@ -2,15 +2,30 @@ import {Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { State } from 'src/app/shared/routing/id-reducer.reducer';
 import { Player } from '../../models/player';
+import { Team } from '../../models/team';
 import { teamsGetAll } from '../../teams/store/actions/teams.action';
+import { selectAllTeams } from '../../teams/store/reducers/teams.reducer';
+import { CLEANTEAMSARENAS } from '../../teams/teams.component';
 import { playerGetById } from './store/actions/players-id.action';
 import { selectedOnePlayerByListTeams } from './store/reducers/players-id.reducer';
 
 export const CLEAN_NULL = filter((valnull:any) => !!valnull)
+
+const MAP_SET_TEAM = map((teams:Team[]) => teams.map((row:Team)=>({id:row.id , name:row.name})));
+
+const MAP_ONE_ROW = map((player:any|Player) =>([{ id:player.teamid, name:player?.team }]));
+const FILTER_ROW = filter((playerrow:any|Player) => {
+    return playerrow !== undefined && playerrow[0].id !== undefined
+});
+
+interface DataLoad {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-players-view',
@@ -20,6 +35,9 @@ export const CLEAN_NULL = filter((valnull:any) => !!valnull)
 export class PlayersViewComponent implements OnInit {
 
   player$ : Observable<Player> = new Observable<Player>();
+  teamSet$: Observable<any|DataLoad[]> = of([]);
+  teamsList$: Observable<any|DataLoad[]> = of([]);
+
   typeView :string = '';
   formPlayer: FormGroup;
 
@@ -31,7 +49,10 @@ export class PlayersViewComponent implements OnInit {
       lastname:[''],
       birthday:[''],
       height:[''],
-      weight:['']
+      weight:[''],
+      photo:[''],
+      team:[''],
+      number:['']
     })
    }
 
@@ -40,6 +61,11 @@ export class PlayersViewComponent implements OnInit {
     this.store.dispatch(teamsGetAll());
     this.store.dispatch(playerGetById());
     this.player$ = this.store.select(selectedOnePlayerByListTeams).pipe(CLEAN_NULL);
+
+    this.teamsList$ = this.store.select(selectAllTeams).pipe(CLEANTEAMSARENAS,MAP_SET_TEAM);
+    this.teamSet$ = this.store.select(selectedOnePlayerByListTeams).pipe(CLEAN_NULL,MAP_ONE_ROW,FILTER_ROW);
+
+
   }
 
   save(){
