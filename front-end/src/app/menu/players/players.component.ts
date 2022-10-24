c8ca947ccb716ada1c419c6e801112e14fb5fc68
Jefferson <jefferson.medina@dhigroupinc.com>
2022-10-24 13:34:03 -0500

    Create search element un players

--- front-end/src/app/menu/players/players.component.ts
+++ front-end/src/app/menu/players/players.component.ts
@@ -9,13 +9,29 @@ import { filter } from 'rxjs';
 import { teamsGetAll } from '../teams/store/actions/teams.action';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
-import { takeUntil } from 'rxjs/operators'
+import { map, takeUntil } from 'rxjs/operators'
 import { UnsubscribeComponent } from 'src/app/shared/unsubscribe/unsubscribe.component';
 import { playerDeleteRow } from './players-view/store/actions/players-delete.action';
 import { DialogTableCardComponent } from 'src/app/shared/components/dialog-table-card/dialog-table-card.component';
 import { setTypeViewPlayerResp } from './store/reducers/type-view-player.reducer';
 import { IFSPACE } from '../arenas/arenas.component';
 import { sendTypePlayerView } from './store/actions/type-view-player.action';
+
+
+export const FILTER_PLAYER = (search:string) =>  map((player:Player[]) => {
+  return player.filter((playerrow:Player)=> playerrow.firstname.toLocaleLowerCase().includes(search.toLowerCase()) || 
+                                    playerrow.lastname.toLocaleLowerCase().includes(search.toLowerCase()) ||     
+                                    playerrow.birthday.toLocaleLowerCase().includes(search.toLowerCase())   ||
+                                    playerrow.country.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.height.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.weight.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.college.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.nbadebut.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.position.toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    String(playerrow.team).toLocaleLowerCase().includes(search.toLowerCase()) ||
+                                    playerrow.number.toLocaleLowerCase().includes(search.toLowerCase()))
+
+})
 
 export const CLEANARRAY =  filter((val:Player[]) => val.length >0)
 
@@ -28,6 +44,7 @@ export class PlayersComponent extends UnsubscribeComponent implements OnInit {
 
   playersList$ : Observable<Player[]> = new Observable<Player[]>();
   typeViewshow$:Observable<string>  = this.store.select(setTypeViewPlayerResp).pipe(IFSPACE);
+  searchTerm: string = '';
 
   constructor(private store :Store<State>,private dialog: MatDialog) { 
     super();
@@ -76,6 +93,15 @@ export class PlayersComponent extends UnsubscribeComponent implements OnInit {
           this.store.dispatch(sendTypePlayerView({ sendviewplayer: confirmed.value }));  
       }
     });
+  }
+
+  searchPlayer(data:string|any):void{    
+    this.playersList$= this.store.select(selectedPlayersWithTeams).pipe(CLEANARRAY,FILTER_PLAYER(data.value));
+  }
+
+  resetSearch():void{
+    this.searchTerm = '';
+    this.playersList$ = this.store.select(selectedPlayersWithTeams).pipe(CLEANARRAY);
   }
 
 }
