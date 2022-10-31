df94c004adc019c800030443b97fbb49be01a13e
Jefferson <jefferson.medina@dhigroupinc.com>
2022-10-31 15:02:54 -0500

    add new url to environment file

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   loginUrl:'https://jefferson-back.informati-k.com/api/login',
   arenaUrl:'http://localhost:3000',
   teamUrl:'http://localhost:3000',
+  playerUrl:'http://localhost:3000',
   playerUrl:'http://localhost:3000'
 };
