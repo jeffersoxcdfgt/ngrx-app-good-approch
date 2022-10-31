210243b2d490d3c19b2d2a08f26e433b4fcd0684
Jefferson <jefferson.medina@dhigroupinc.com>
2022-10-31 15:01:03 -0500

    create detaill view regular season games

--- front-end/src/app/menu/regular-season-games/regular-season-games.component.ts
+++ front-end/src/app/menu/regular-season-games/regular-season-games.component.ts
@@ -17,12 +17,49 @@ export const CLEANARRAY =  filter((val:ReagularSeasonGame[]) => val.length >0)
 export class RegularSeasonGamesComponent implements OnInit {
 
   regularseasongamesList$ : Observable<ReagularSeasonGame[]> = new Observable<ReagularSeasonGame[]>();
+  selectedData:string[] = [];
+  selectedDataDetail:string[] = [];
+  allElementsString = 'expand-all-details js-expand-all-details collapsed link-icon'
 
   constructor(private store :Store<State>) { }
 
   ngOnInit(): void {
     this.store.dispatch(regularseasongamesGetAll());
     this.regularseasongamesList$ = this.store.select(selectAllRegularSeasonGames).pipe(CLEANARRAY);
+  }
+
+  modelChangeFn(id:number|any):void{    
+    this.selectedData[id] =  this.selectedData[id] === 'expand-details collapsed js-expand-details link-icon' ? 
+    'expand-details expanded js-expand-details link-icon':'expand-details collapsed js-expand-details link-icon'
+
+    this.selectedDataDetail[id] =  this.selectedDataDetail[id] === 'none' ? 'table-row': 'none'
+  }
+
+  setClass(arrayData:string[],id:number):string{
+    if(arrayData[id] === undefined){
+      arrayData[id]='expand-details collapsed js-expand-details link-icon'
+    }
+    return arrayData[id];
+  }
+
+  setClassDetail(arrayData:string[],id:number):string{
+    if(arrayData[id] === undefined){
+      arrayData[id]='none'
+    }
+    return arrayData[id];
+  }
+
+  allElements():void{
+    if(this.allElementsString === 'expand-all-details js-expand-all-details collapsed link-icon'){
+        this.allElementsString = 'expand-all-details js-expand-all-details expanded link-icon'
+        this.selectedData = this.selectedData.map((_) => 'expand-details expanded js-expand-details link-icon')
+        this.selectedDataDetail = this.selectedDataDetail.map((_)=> 'table-row' )
+    }
+    else{
+        this.allElementsString = 'expand-all-details js-expand-all-details collapsed link-icon'
+        this.selectedData = this.selectedData.map((_) => 'expand-details collapsed js-expand-details link-icon')
+        this.selectedDataDetail = this.selectedDataDetail.map((_)=> 'none' )
+    } 
   }
 
 }
