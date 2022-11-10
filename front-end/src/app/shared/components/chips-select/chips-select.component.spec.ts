ad4a3732668b5ee250f9efcf83a2bef5c3de9b32
Jefferson <jefferson.medina@dhigroupinc.com>
2022-11-10 17:28:31 -0500

    adding new unit test cases

--- front-end/src/app/shared/components/chips-select/chips-select.component.spec.ts
+++ front-end/src/app/shared/components/chips-select/chips-select.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -45,5 +46,31 @@ describe('ChipsSelectComponent', () => {
     component.remove({id:'1'})
     expect(component.dataset.length ===1).toBeTrue()   
   });
+
+  it('Call selected to get what element was selected increase number elements',()=>{
+  const event:any|MatAutocompleteSelectedEvent ={
+    option:{
+      value:{
+        id:1
+      }
+    }
+  };
+    component.dataset = [{id:'1',name:'element1'},{id:'2',name:'element2'}]
+    component.selected(event);
+    expect(component.dataset.length).toBe(3);
+  })
+
+
+  it('Call displayFn get the same name', ()=>{    
+    const data = {id:'1',name:'element1'}
+    const res = component.displayFn(data)
+    expect(res).toBe('element1')
+  })
+
+  it('Call displayFn get the "" when is null', ()=>{    
+    const data:any|string = undefined
+    const res =component.displayFn(data)
+    expect(res).toEqual('')
+  })
 
 });
