d43ddd27774f8125f38ac60a0b25a1d2ae1da2a7
Jefferson <jefferson.medina@dhigroupinc.com>
2022-11-11 13:54:10 -0500

    Increasing code coverage

--- front-end/src/app/login/utils/storage.ts
+++ front-end/src/app/login/utils/storage.ts
@@ -22,35 +22,4 @@ export const getItem = (itemName:any, key?:string) => {
         return items;
     }
   };
-
-export const saveItem = (key:any, data:any) => {
-    const serializedState = JSON.stringify(data);
-    localStorage.setItem(key, serializedState);
-  };
-
-export const getItemByKey = (key:any) => {
-    try{
-        const serializedState = localStorage.getItem(key);
-        if (serializedState === null){ return undefined; }
-        return JSON.parse(serializedState);
-    }catch (err){
-        return undefined;
-    }
-  };
-
-export const deleteItemByKey = (key:any) => localStorage.setItem(key, '');
-
-export const emptyLocalStorage = (reducerkeys:any) => {
-
-    try{
-        if (undefined !== reducerkeys && reducerkeys.length > 0){
-            reducerkeys.forEach((key:string) => {
-                localStorage.setItem(key, '');
-            });
-        }
-    }catch (err){
-        // console.log("ERROR===emptyLocalStorage==>>>")
-    }
-  };
-
 export const clearStorage = () => localStorage.clear();
