2d4b65159f99964b9d2e8c7ca09b99b7b883fbbc
Jefferson <jefferson.medina@dhigroupinc.com>
2022-11-15 14:36:10 -0500

    creating Integration testing examples

---
+++ front-end/src/app/shared/not-found/not-found.component.spec.ts
@@ -0,0 +1,50 @@
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { PageNotFoundComponent } from './not-found.component';
+
+fdescribe('PageNotFoundComponent', () => {
+
+ let component: PageNotFoundComponent;
+ let fixture: ComponentFixture<PageNotFoundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        PageNotFoundComponent
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the not-found', () => {
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'was created'`, () => { 
+    const debugElement:DebugElement = fixture.debugElement.query(By.css('h2'))
+    expect(debugElement).toBeTruthy()
+  });
+
+  it(`should have as title 'Page not found'`, () => { 
+    const debugElement:DebugElement = fixture.debugElement.query(By.css('h2'))
+    expect(debugElement.nativeElement.innerText).toEqual('Page not found')
+  });
+
+  it(`should have as message 'was created'`, () => { 
+    const debugElement:DebugElement = fixture.debugElement.query(By.css('h4'))
+    expect(debugElement).toBeTruthy()
+  });
+
+  it(`should have as message 'Sorry, This page is not available'`, () => { 
+    const debugElement:DebugElement = fixture.debugElement.query(By.css('h4'))
+    expect(debugElement.nativeElement.innerText).toEqual('Sorry, This page is not available')
+  });
+
+
+});
