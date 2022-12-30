0c2e1d226bd2ee12eae9eb611b2bd1cb15a5554a
Jefferson <jefferson.medina@dhigroupinc.com>
2022-12-30 11:59:35 -0500

    Improving css syle for print view

--- front-end/src/app/menu/arenas/arenas-print/arenas-print.component.ts
+++ front-end/src/app/menu/arenas/arenas-print/arenas-print.component.ts
@@ -19,9 +19,21 @@ export class ArenasPrintComponent implements OnInit {
   constructor(private store :Store<State>){}
 
   ngOnInit(): void {
+    this.removejscssfile("main_cerulean.css", "css");
     this.store.dispatch(arenasGetAll());
     this.arenasList$ = this.store.select(selectAllArenas).pipe(CLEANARRAY);
   }
+
+
+  removejscssfile(filename:any, filetype:any){
+    const targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    const allsuspects:any=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+ }
   
 
 }
