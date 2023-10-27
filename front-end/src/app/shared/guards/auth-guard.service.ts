66b127199eaf20e7bb9cb3db28514b874000009e
jefferson <jefferson.medina@sourcemeridian.com>
2023-10-27 12:35:22 -0500

    Update angular 16

--- front-end/src/app/shared/guards/auth-guard.service.ts
+++ front-end/src/app/shared/guards/auth-guard.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
   constructor(
     public router: Router
   ) { }
