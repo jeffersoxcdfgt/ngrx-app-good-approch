6258d53723cd7bde1b1ab92fff20d28e9e50b5fd
Jefferson Medina <xcdfgt@gmail.com>
2023-11-11 16:40:46 -0500

    update cypress automation test

--- automation/cypress/e2e/arenas.cy.js
+++ automation/cypress/e2e/arenas.cy.js
@@ -38,6 +38,45 @@ describe('Arenas Testing', () => {
 
        cy.get('[data-save="save"]').click().then(()=>{
         cy.get('table tbody tr').should('have.length', 8)
+       }) 
+    })
+
+    it('Updating arenas row', () => {   
+      cy.get('a').get(`[routerlink="${ARENAS_LINK}"]`).click()
+      cy.get('a').get(`[ng-reflect-router-link="${ARENAS_LINK}/edit/1"]`).click()
+
+      cy.get('app-input-custom').get('input').get('[placeholder="Arena title"]').clear().type('jeff') 
+      cy.get('app-input-custom').get('input').get('[placeholder="Capacity"]').clear().type('11111') 
+
+       // Set text to tinyMce        
+       cy.window().should('have.property', 'tinymce')   
+       cy.wait(1000).then(() => { 
+            const win = cy.state('window')
+            win.tinymce.activeEditor.setContent('About description cypress')
+      })
+      
+      cy.get('app-logo-custom').get('input[type=file]').get('[id="logo"]').selectFile({
+        contents: Cypress.Buffer.from('file contents'),
+        fileName: 'logo.png',
+        lastModified: Date.now(),
+      }) 
+
+      cy.get('app-logo-custom').get('input[type=file]').get('[id="photo"]').selectFile({
+        contents: Cypress.Buffer.from('file contents'),
+        fileName: 'photo.png',
+        lastModified: Date.now(),
+      })
+
+      cy.get('[data-save="save"]').click().then(()=>{
+        cy.get('table')
+          .should('have.id', 'table-arenas')
+          .find('tbody tr:nth-child(1) > td')
+          .contains('jeff')
+
+          cy.get('table')
+          .should('have.id', 'table-arenas')
+          .find('tbody tr:nth-child(1) > td')
+          .contains('11111')        
        })  
 
     })
