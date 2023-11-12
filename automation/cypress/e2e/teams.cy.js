a684d9e34e47a56000824ee3d430cdbe9882ae3e
Jefferson Medina <xcdfgt@gmail.com>
2023-11-11 19:34:28 -0500

    Update automation test

--- automation/cypress/e2e/teams.cy.js
+++ automation/cypress/e2e/teams.cy.js
@@ -6,10 +6,10 @@ describe('Teams Testing', () => {
       LoginFunction('admin@admin.com','admin')
     })
 
-    it('Adding arenas row', () => {        
+    it('Adding teams row', () => {        
        cy.get('a').get(`[routerlink="${TEAMS_LINK}"]`).click();
        cy.get('a').get(`[routerlink="${TEAMS_LINK}/add"]`).click(); 
-       cy.get('app-input-custom').get('input').get('[placeholder="Team Name"]').type('Team1');
+       cy.get('app-input-custom').get('input').get('[placeholder="Team Name"]').type('Team new');
        cy.get('app-input-custom').get('input').get('[placeholder="Founded"]').type('1998') ;
 
         // Set text to tinyMce        
@@ -45,5 +45,54 @@ describe('Teams Testing', () => {
           cy.get('table tbody tr').should('have.length', 6)
         })
   
+    })
+
+    it('Updating teams row', () => { 
+      cy.get('a').get(`[routerlink="${TEAMS_LINK}"]`).click()
+      cy.get('a').get(`[ng-reflect-router-link="${TEAMS_LINK}/edit/1"]`).click() 
+
+      cy.get('app-input-custom').get('input').get('[placeholder="Team Name"]').clear().type('jeff');
+      cy.get('app-input-custom').get('input').get('[placeholder="Founded"]').clear().type('2023');
+
+         // Set text to tinyMce        
+      cy.window().should('have.property', 'tinymce')   
+         cy.wait(1000).then(() => { 
+         const win = cy.state('window')
+         win.tinymce.activeEditor.setContent('About description cypress')
+      }) 
+
+      cy.get('app-logo-custom').get('input[type=file]').get('[id="logo"]').selectFile({
+        contents: Cypress.Buffer.from('file contents'),
+        fileName: 'logo.png',
+        lastModified: Date.now(),
+      }) 
+
+      cy.get('app-chips-select')
+      .get('input')
+      .get('[formcontrolname="arena"]')
+      .eq(0)
+      .click({multiple: true })
+      .should('be.visible')
+      .as('relatedArenaAutocomplete').first().within(() => {
+        cy.get('input')
+        .type('{selectAll}')
+        .type('{backspace}')
+        .type('Amway')
+        .type('{downarrow}')
+        .type('{enter}');
+      });
+
+      cy.get('app-chips-select')
+      .get('select')
+      .select('Central').should('have.value', 'Central')
+
+      cy.get('[data-save="save"]').click().then(()=>{
+        cy.get('table')
+          .contains('jeff')
+          
+        cy.get('table')
+          .contains('2023')  
+       }) 
+
     })
 })
